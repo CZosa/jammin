465107aea1c72bb850f63d072c9e0b0127d24f4a
Chris Zosa <zosachris@gmail.com>
2021-03-16 15:48:56 -0700

    component displayes genre

--- pages/venues/[id].js
+++ pages/venues/[id].js
@@ -1,12 +1,19 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const Venue = () => {
+  //gets query params
+  const router = useRouter();
+
+  const { id } = router.query;
+
   return (
     <div>
       <Head>
         <title>Venue</title>
       </Head>
-      <div>Venues</div>
+      <h1>Venue: {id}</h1>
+      <div>{id} </div>
     </div>
   );
 };
